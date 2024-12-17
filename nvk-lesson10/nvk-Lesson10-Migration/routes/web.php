9303652e19149881443041522ce3f6314818fa56
AniKMizu2262 <khainguyenangelz10072005@gmail.com>
2024-12-17 15:43:12 +0700

    NVK - Project 1 - Lesson 11

--- nvk-lesson10/nvk-Lesson10-Migration/routes/web.php
+++ nvk-lesson10/nvk-Lesson10-Migration/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\nvkNhaCCController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// NhaCC 
+Route::get('/nvkNhaCC',[nvkNhaCCController::class,'list'])->name('nvkNhaCC.List');
